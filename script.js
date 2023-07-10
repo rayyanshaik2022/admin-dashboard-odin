f80662dd54e6636d191961dd112542aa12965a7d
Rayyan Shaik <56854894+rayyanshaik2022@users.noreply.github.com>
2023-07-09 22:27:07 -0400

    Sleep tracker card

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ sideBarBtns.forEach((btn) => {
             btn.classList.add("selected");
             sideBarBtnActive = btn;
         });
-    }   
+    }
 });
 
 // Git Commit Card
@@ -18,18 +18,42 @@ commitCounter = document.querySelector("#commit-counter");
 commitTag = document.querySelector("#commit-tag");
 commitDesc = document.querySelector("#commit-tag-desc");
 
-let counter = Math.floor( Math.random() * 70 ) + 12;
-let tagPercent = Math.floor( Math.random() * 120 ) + 5;
+let counter = Math.floor(Math.random() * 70) + 12;
+let tagPercent = Math.floor(Math.random() * 120) + 5;
 
 commitCounter.textContent = counter;
 commitTag.textContent = `+${tagPercent}%`;
 
 // Random chance for negative
 if (Math.random() < 0.4) {
-    tagPercent = Math.floor( Math.random() * 25 ) + 3;
+    tagPercent = Math.floor(Math.random() * 25) + 3;
     commitTag.textContent = `-${tagPercent}%`;
     commitDesc.textContent = "decrease from yesterday";
 
     commitTag.classList.remove("green");
     commitTag.classList.add("red");
 }
+
+// Chart logic
+const line_chart_data = {
+    labels: ["July", "August", "September", "October", "November", "December"],
+    datasets: [
+        {
+            label: "Average Hours Slept",
+            backgroundColor: "rgb(139, 211, 250)",
+            borderColor: "rgb(139, 211, 250)",
+            data: [8.7, 8.1, 6.4, 7.2, 6.0, 5.9,3],
+        },
+    ],
+};
+
+const line_chart_config = {
+    type: "line",
+    data: line_chart_data,
+    options: { responsive: true },
+};
+
+const lineChart = new Chart(
+    document.getElementById("sleep-chart"),
+    line_chart_config
+);
